fa8e023aac467fc0d2313e5784f9d4bacce8faa3
Adam Fekete <adam.fekete@unamur.be>
2021-09-16 03:55:14 +0200

    LDR

--- src/polarizability.jl
+++ src/polarizability.jl
@@ -14,81 +14,82 @@ The Clausius-Mossoti polarizability of dipoles:
 - `eps`: complex permitivirty of dipole
 """
 function polarizability_CM(d, eps)
-    return 3d^3 / 4pi * (eps - 1) / (eps +2)
+    return 3d^3 / 4pi * (eps - 1) / (eps + 2)
 end
 # alpha_CM(eps, d) = 3d^3 / 4pi * (eps - 1) / (eps +2)
 
 
+"""
+LDR: Lattice Dispersion Relation
 
-function polarizability_LDR93(d, eps, kvec, E0)
+d: dipole "lattice" spacing
+eps: complex permittivity
+k_vec: wavevectors
+E0: complex incident field amplitude
+"""
+function polarizability_LDR(d, eps, kvec, E0)
     b1 = -1.8915316
     b2 =  0.1648469
     b3 = -1.7700004
 
+    # wavenumber
     k = norm(kvec)
+
+    # a_hat: incident direction (unit vector)
     a_hat = normalize(kvec)
+
+    # e_hat: polarisation state (unit vector)
     e_hat = normalize(E0)
+
     S = sum(@. (a_hat * e_hat)^2)
 
     # First compute Clausius-Mossotti polarizability:
     alpha_CM = polarizability_CM(d, eps)
 
-    return alpha_CM / (1 + alpha_CM / d^3 * ((b1 + eps * b2 + eps * b3 * S) * (k * d)^2 - 2/3 * im * (k * d)^3))
+    return alpha_CM / (1 + alpha_CM / d^3 * ((b1 + eps * b2 + eps * b3 * S) * (k * d)^2 - 2 / 3 * im * (k * d)^3))
 
 end
 
 
+"""
+LDR93: Lattice Dispersion Relation
 
-# function polarizability_LDR93_5(eps, d, kvec, E0)
-#     b1 = -1.8915316
-#     b2 =  0.1648469
-#     b3 = -1.7700004
-#
-#     k = norm(kvec)
-#     a_hat = normalize(kvec)
-#     e_hat = normalize(E0)
-#     S = sum(@. (a_hat * e_hat)^2)
-#
-#     # First compute Clausius-Mossotti polarizability:
-#     alpha_CM = polarizability_CM(d, eps)
-#
-#     # Determine polarizability by requiring that infinite lattice of
-#     # dipoles have dipersion relation of continuum.
-#     alpha_inf = alpha_CM / (1 + alpha_CM / d^3 * (b1 + eps * (b2 + b3 * S)) * (k * d)^2)
-#
-#     # Radiative-reaction correction:
-#     alpha = k^3 * alpha_inf/(1 -  2/3 * im * k^3* alpha_inf)
-#     #   NOTE: k^3 ???? d^3 ????
-#     return alpha
-#
-# end
-# alph = polarizability_LDR93_5(d, eps, kvec, E0)
-#
-# function polarizability_LDR93_4(d, eps, kvec, E0)
-#     b1 = -1.8915316
-#     b2 =  0.1648469
-#     b3 = -1.7700004
-#
-#     k = norm(kvec)
-#     a_hat = normalize(kvec)
-#     e_hat = normalize(E0)
-#     S = sum(@. (a_hat * e_hat)^2)
-#
-#     # First compute Clausius-Mossotti polarizability:
-#     alpha_CM = polarizability_CM(d, eps)
-#
-#     # Determine polarizability by requiring that infinite lattice of
-#     # dipoles have dipersion relation of continuum.
-#     alpha_inf = alpha_CM / (1 + alpha_CM/ d^3  * (b1 + eps * ( b2 + b3 * S)) * (k * d)^2)
-#
-#     # Radiative-reaction correction:
-#     alpha =  alpha_inf/(1 - 2/3 * im * (k * d)^3 * alpha_inf)
-#
-#     return alpha
-#
-#
-# end
-# alph = polarizability_LDR93_4(d, eps, kvec, E0)
+d: dipole "lattice" spacing
+eps: complex permittivity
+k_vec: wavevectors
+E0: complex incident field amplitude
+
+https://doi.org/10.1086/172396
+https://doi.org/10.1364/JOSAA.11.001491
+"""
+function polarizability_LDR94(eps, d, kvec, E0)
+    b1 = -1.8915316
+    b2 =  0.1648469
+    b3 = -1.7700004
+
+    # wavenumber
+    k = norm(kvec)
+
+    # a_hat: incident direction (unit vector)
+    a_hat = normalize(kvec)
+
+    # e_hat: polarisation state (unit vector)
+    e_hat = normalize(E0)
+
+    S = sum((a_hat[i] * e_hat[i])^2 for i in 1:3)
+
+    # First compute Clausius-Mossotti polarizability:
+    alpha_CM = polarizability_CM(d, eps)
+
+    # Determine polarizability by requiring that infinite lattice of
+    # dipoles have dipersion relation of continuum.
+    alpha_inf = alpha_CM / (1 + alpha_CM / d^3 * (b1 + eps * (b2 + b3 * S)) * (k * d)^2)
+
+    # Radiative-reaction correction:
+    alpha = alpha_inf / (1 -  2 / 3 * im * k^3 * alpha_inf)
+
+    return alpha
+end
 
 
 
