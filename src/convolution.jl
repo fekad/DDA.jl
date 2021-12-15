bcc09bd663fc4bf334dcd68d9baedb28af1e7e99
fekad <adam@fekete.co.uk>
2021-12-15 14:56:29 +0100

    cleunup + future

--- src/convolution.jl
+++ src/convolution.jl
@@ -1,3 +1,5 @@
+# TODO: adding parametic types like here: https://github.com/JuliaMatrices/ToeplitzMatrices.jl/blob/master/src/ToeplitzMatrices.jl
+# TODO: improve structure: Array of symmetric tensors
 """
     TensorConvolution(...)
 
@@ -6,11 +8,12 @@ A preplanned, circular convolution operator on an M × N matrix of data of type 
 # Fields
 - `Ĝ`: DFT coefficients of the convolution kernel
 - `F`: plan for FFT operator
-- `paddedSpace`: scratch space to zero-pad the input matrix
-- `Â`: scratch space to store the DFT coefficients of the zero-padded input matrix
+- `inds`: Cartesian indecies of each dipole
+- `alphas`: Polirisability of each dipole
+- `tmp`: scratch space to store the zero-pad the input matrix
 
 # Constructors:
-- `TensorConvolution(G::Matrix{T})
+- `TensorConvolution(grid::CartesianGrid, occ, k_norm, alphas)
 """
 struct TensorConvolution{P<:AbstractFFTs.Plan{ComplexF64}}
     Ĝ::Array{ComplexF64,4}
@@ -76,7 +79,7 @@ function LinearAlgebra.mul!(y::StridedVector, A::TensorConvolution, x::StridedVe
         A.tmp[3, i, j, k] = A.Ĝ[3, i, j, k] * E[1] + A.Ĝ[5, i, j, k] * E[2] + A.Ĝ[6, i, j, k] * E[3] # z component
     end
 
-    inv(A.F) * A.tmp # inverse FFT
+    A.F \ A.tmp # inverse FFT
 
     for i in eachindex(y)
         y[i] = A.tmp[:, A.inds[i]] .+ 1 / A.alphas * x[i]
@@ -113,3 +116,116 @@ function extend!(Ĝ::Array{ComplexF64,4}, Nx, Ny, Nz)
     Ĝ[:, Nx+2:2Nx, Ny+2:2Ny, Nz+2:2Nz] = Ĝ[:, Nx:-1:2, Ny:-1:2, Nz:-1:2]   # xyz
 
 end
+
+# Future improvements:
+
+# using Tensors
+# using Tensorial
+
+# function TensorConvolution(grid::CartesianGrid, occ, k_norm, alphas)
+#     T = SymmetricTensor{2,3,ComplexF64} # T((1,2,3,4,5,6)) # using Tenors
+#     T = SymmetricSecondOrderTensor{3,ComplexF64} # T(1,2,3,4,5,6) # using Tensorial
+#
+#     Nx, Ny, Nz = size(grid)
+#     Ĝ = Array{T}(undef, 2Nx, 2Ny, 2Nz)  # BlockArrays ArraysofStruct # StaticArrays
+#     for i in eachindex(Ĝ)
+#         r_ij = grid[i]
+#         Ĝ[i] = DDA.calc_Ajk(k_norm, r_ij)
+#     end
+#
+#     # TODO: merging this two together and doing fft by slices (1-6) and trimming the data afterwards
+#     extend!(Ĝ, Nx, Ny, Nz)
+#     fft!(Ĝ, 2:4)  # This doesn't work on arrays of tensors
+#
+#     tmp = zeros(ComplexF64, 2Nx, 2Ny, 2Nz)
+#     F = FFTW.plan_fft!(tmp, 2:4) # reinterpret??
+#
+#     inds = findall(occ)
+#     TensorConvolution2{typeof(F)}(Ĝ, F, inds, alphas, tmp)
+# end
+
+# function LinearAlgebra.mul!(y::StridedVector, A::TensorConvolution2, x::StridedVector)
+#     x = reinterpret(SVector{3,ComplexF64}, x)
+#     y = reinterpret(SVector{3,ComplexF64}, y)
+#
+#     tmp = padded(A.inds, x)
+#
+#     A.F * tmp # FFT
+#
+#     # !! Matrix vector multiplication in the Fourier space
+#     for i in eachindex(A.Ĝ)
+#         tmp[i] = A.Ĝ[i] * tmp[i]
+#     end
+#
+#     A.F \ tmp # inverse FFT
+#
+#     # TODO: alpha must be a tensor
+#     for i in eachindex(y)
+#         y[i] = tmp[A.inds[i]] .+ inv(A.alpha[i]) * x[i]
+#     end
+#
+#     return y
+#
+# end
+
+# function extend!(Ĝ::Array{...}, Nx, Ny, Nz)
+#
+#     T = eltype(Ĝ)
+#
+#
+#     for k = 1:Nz, j = 1:Ny
+#         Ĝ[Nx+1, j, k] = zero(T)
+#     end
+#
+#     for k = 1:Nz, i = 1:Nx-1
+#         Ĝ[i, Ny+1, k] = zero(T)
+#     end
+#
+#     for j = 1:Ny, i = 1:Nx-1
+#         Ĝ[i, j, Nz+1] = zero(T)
+#     end
+#
+#
+#     sym = [-1 1 1]   # x
+#     g = T(sym'*sym)
+#     for k = 1:Nz, j = 1:Ny, i = 1:Nx-1
+#         Ĝ[Nx+1+i, j, k] = Ĝ[Nx-i, j, k] * g
+#     end
+#
+#     sym = [1 -1 1]   # y
+#     g = T(sym'*sym)
+#     for k = 1:Nz, j = 1:Ny-1, i = 1:Nx
+#         Ĝ[i, Ny+1+j, k] = Ĝ[i, Ny-j, k] * g
+#     end
+#
+#     sym = [1 1 -1]   # z
+#     g = T(sym'*sym)
+#     for k = 1:Nz-1, j = 1:Ny, i = 1:Nx
+#         Ĝ[i, j, Nz+1+k] = Ĝ[i, 1:Ny, Nz-k] * g
+#     end
+#
+#     sym = [-1 -1 1]   # xy
+#     g = T(sym'*sym)
+#     for k = 1:Nz, j = 1:Ny-1, i = 1:Nx-1
+#         Ĝ[Nx+1+i, Ny+1+j, k] = Ĝ[Nx-i, Ny-j, k] * g
+#     end
+#
+#     sym = [-1 1 -1]   # xz
+#     g = T(sym'*sym)
+#     for k = 1:Nz-1, j = 1:Ny, i = 1:Nx-1
+#         Ĝ[Nx+1+i, j, Nz+1+k] = Ĝ[Nx-i, j, Nz-k] * g
+#     end
+#
+#     sym = [1 -1 1]   # yz
+#     g = T(sym'*sym)
+#     for k = 1:Nz-1, j = 1:Ny-1, i = 1:Nx
+#         Ĝ[Nx+i, Ny+1+j, Nz+1+k] = Ĝ[i, Ny-j, Nz-k] * g
+#     end
+#
+#     sym = [1 1 1]   # xyz
+#     g = T(sym'*sym)
+#     for k = 1:Nz-1, j = 1:Ny, i = 1:Nx
+#         Ĝ[Nx+1+i, Ny+1+j, Nz+1+k] = Ĝ[Nx-i, Ny-j, Nz-k]
+#     end
+#
+# end
