876e3e987d41529c07d92400c80789ce01179ab5
fekad <adam@fekete.co.uk>
2021-12-22 02:05:39 +0100

    eels

--- src/incidentfield.jl
+++ src/incidentfield.jl
@@ -1,3 +1,5 @@
+abstract type IncindentField <: AbstractField end
+
 raw"""
 # Incident electric field
 
@@ -13,9 +15,6 @@ Arbitrary incident electric field:
 Visualisation:
 - Polarisation state can be represented as a point on the Bloch (Poincare) sphere.
 """
-
-
-abstract type IncindentField <: AbstractField end
 
 
 @doc raw"""
@@ -66,128 +65,48 @@ Arbitrary direction:
 ```
 """
 struct PlaneWave <: IncindentField
-    k::Float64
-    e::SVector{2,ComplexF64}
-    θ::Float64
-    ϕ::Float64
-end
-
-# E = PlaneWave(1, (1, 1im), 1, 1)
-
-function params(E::PlaneWave)
-    R = RotZY(E.θ, E.ϕ)
-    E₀ = R[:,1:2] * E.e
-    kvec = R[:,3] * E.k
-    return kvec, E₀
-end
-
-
-function field(E::PlaneWave, r::AbstractVector)
-    return E₀ * exp(im * dot(kvec, r))
-end
-
-
-struct PlaneWave2 <: IncindentField
     kvec::SVector{3,Float64}
     E₀::SVector{3,ComplexF64}
 end
 
-# syntax sugar
-function PlaneWave2(k ,e, θ, ϕ)
+# "minimal" representation of a plane wave
+function PlaneWave(k::Float64, e::SVector{2,ComplexF64}, θ::Float64, ϕ::Float64)
     R = RotZY(θ, ϕ)
-    E₀ = R[:,1:2] * e
-    kvec = R[:,3] * k
-    return PlaneWave2(kvec, E₀)
+    E₀ = R[:, 1:2] * e
+    kvec = R[:, 3] * k
+    return PlaneWave(kvec, E₀)
 end
 
-function field(f::PlaneWave2, r::AbstractVector{Point3{T}}) where T
-    E = similar(r, SVector{3, ComplexF64})
-    for i in 1:length(r)
+field(f::PlaneWave, r::Point3) = f.E₀ * exp(im * dot(f.kvec, r))
+
+
+K₀(z) = besselk(0, z)
+K₁(z) = besselk(1, z)
+
+struct EELS <: IncindentField
+    k::Float64 # wavenumber
+    v::Float64 # charge velocity (propagate along z direction)
+    origin::SVector{2,Float64} # position in the xy plane
+end
+
+function field(f::EELS, r::Point3)
+    d = r[1:2] - f.origin
+    dnorm = norm(d)
+
+    A = q.val / (2π * ε_0.val) * f.k / f.v * exp(1im * f.k * r[3])
+
+    return [
+        A * d[1] / dnorm * K₁(f.k * dnorm),
+        A * d[2] / dnorm * K₁(f.k * dnorm),
+        -A * 1im * K₀(f.k * dnorm)
+    ]
+end
+
+
+function field(f::IncindentField, r::AbstractVector{Point3{T}}) where {T}
+    E = similar(r, SVector{3,ComplexF64})
+    for i = 1:length(r)
         E[i] = field(f, r[i])
     end
     E
 end
-
-
-field(f::PlaneWave2, r::Point3) = f.E₀ * exp(im * dot(f.kvec, r))
-
-# function field(E::PlaneWave, r::AbstractVector{AbstractVector})
-#     out = Array{ComplexF64}(undef, (3, length(r)))
-#     for i in eachindex(r)
-#         out[:,i] = field(E, r[i])
-#     end
-#     return out
-# end
-
-
-function field(E::PlaneWave, r::AbstractArray)
-
-    # @boundcheck if size(r, 1) != 3
-    #     throw("dimension error")
-    # end
-
-    out = Array{ComplexF64}(undef, (3, length(r)))
-    for i in eachindex(r)
-        out[:,i] = field(E, r[i])
-    end
-    return out
-end
-
-
-
-
-
-# """
-# Incident wave:
-# ```math
-# E_{inc,j} = E_0 \exp(-i k r_j - i \omega t)
-# ```
-# """
-#
-# https://discourse.julialang.org/t/struct-of-arrays-soa-vs-array-of-structs-aos/30015/16
-#
-# # polarization:
-# # left and right hand circular polarized VS the orthonormal Cartesian vectors ex and ey
-# struct PlaneWave <: Field
-#     E₀::SVector{3,Float64}
-#     k::SVector{3,Float64}
-# end
-#
-# function field(r, k, E₀)
-#     return E₀ * exp(-im * dot(k, r))
-# end
-#
-# function field(r, E::PlaneWave)
-#     return field(r, E.E₀, E.k)
-# end
-#
-# function field(r::Vector{SVector{3, Float64}}, e::PlaneWave)
-#     out = Array{ComplexF64}(undef, (3, length(r)))
-#     for i in eachindex(r)
-#         out[:,i] = field(r[i], e)
-#     end
-#     return out
-# end
-#
-# # E_inc(r, E::PlaneWave) = E.E0 * exp(-im * E.k)
-# # E_inc_td(t, r, E::PlaneWave, omega) = field(E) * exp(im * omega * t)
-# #
-# # k = [0 0 1]
-# # # u = [0,0,1] polarisibility
-# # E_inc(r, E_0, k) = E_0 * exp(-im * dot(k,r))
-# #
-#
-#
-# # function E_inc(E0, kvec, r)
-# #     Ei = zeros(ComplexF64, 3, length(r))
-# #
-# #     for (i, ri) in enumerate(r)
-# #         Ei[:,i] = E0 .* exp.(im * dot(kvec, ri))
-# #     end
-# #
-# #     return reshape(Ei, :)
-# # end
-
-
-
-
