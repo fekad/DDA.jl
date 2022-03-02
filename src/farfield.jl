cd6d3f881f444f86547172997d87f083410869a7
fekad <adam@fekete.co.uk>
2022-03-02 12:07:46 +0100

    cleanup, alphas

--- src/farfield.jl
+++ src/farfield.jl
@@ -5,15 +5,23 @@ $$
 C_{abs} = \frac{4 \pi k}{|E_0|^2} \sum \limits_{j=1}^{N} \left\{ \mathrm{Im}(P_j \cdot (\alph_j^{-1}) \cdot P_j^*) - \frac{2}{3} k^3|P_j|^2 \right\}
 $$
 """
+# function C_abs(k, E0, P, alph)
+#     c = zero(Float64)
+#     for i in 1:length(P)
+#         c += -imag(dot(P[i], 1 / alph, P[i])) - 2 / 3 * k^3 * norm(P[i])^2
+#     end
+#     return 4π * k / norm(E0)^2 * c
+# end
+
 function C_abs(k, E0, P, alph)
-    c = zero(Float64)
-    for i in 1:length(P)
-        c += -imag(dot(P[i], 1 / alph, P[i])) - 2 / 3 * k^3 * norm(P[i])^2
+    T = Float64
+    c = zero(T)
+    for i in eachindex(P)
+        c += -imag(dot(P[i], inv(alph[i]), P[i])) - 2 / 3 * k^3 * norm(P[i])^2
     end
-    return 4π * k / norm(E0)^2 * c
+    return 4π * k / E0^2 * c
 end
 
-C_abs(sol::GridSolution) = C_abs(norm(sol.prob.Einc.kvec), norm(sol.prob.Einc.E₀), sol.P, sol.alphas)
 
 
 
@@ -25,14 +33,13 @@ $$
 """
 # C_ext(k, E0, Ei, P) = 4π * k / norm(E0)^2 * imag(dot(Ei, P))
 function C_ext(k, E0, Einc, P)
-    c = zero(Float64)
-    for j = 1:length(P)
+    T = Float64
+    c = zero(T)
+    for j in eachindex(P)
         c += imag(dot(Einc[j], P[j]))
     end
-    return 4π * k / norm(E0)^2 * c
+    return 4π * k / E0^2 * c
 end
-
-C_ext(sol::GridSolution)= C_ext(norm(sol.prob.Einc.kvec), norm(sol.prob.Einc.E₀), sol.Einc, sol.P)
 
 @doc raw"""
 The scattering cross section
@@ -41,5 +48,3 @@ C_{sca} = C_{ext} - C_{abs}
 $$
 """
 C_sca(k, E0, Einc, P, alph) = C_ext(k, E0, Einc, P) - C_abs(k, E0, P, alph)
-
-C_sca(sol::GridSolution) = C_ext(sol) - C_abs(sol)
