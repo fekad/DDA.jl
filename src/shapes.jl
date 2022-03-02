aece0e19c1a5b1b191ad6f2d3add613481ddf7bd
fekad <adam@fekete.co.uk>
2022-03-02 14:30:59 +0100

    types

--- src/shapes.jl
+++ src/shapes.jl
@@ -1,9 +1,10 @@
+abstract type AbstractShape{Dim,T} end
 
-struct Dipole <: AbstractShape
-    origin::Point3{Float64}
+struct Dipole{Dim,T} <: AbstractShape{Dim,T}
+    origin::StaticVector{Dim,T}
 end
 
-function dipoles(g::CartesianGrid{T,N}, s::Dipole) where {T,N}
+function dipoles(g::CartesianGrid{Dim,T}, s::Dipole{Dim,T}) where {Dim,T}
 
     ind = @. round(Int, (s.origin - g.origin) / g.spacing + 1)
     inds = [CartesianIndex(Tuple(ind)),]
@@ -13,54 +14,54 @@ function dipoles(g::CartesianGrid{T,N}, s::Dipole) where {T,N}
 end
 
 
-struct Sphere <: AbstractShape
-    origin::Point3{Float64}
-    radius::Float64
+struct Sphere{Dim,T} <: AbstractShape{Dim,T}
+    origin::StaticVector{Dim,T}
+    radius::T
 end
 
-function Base.in(p::Point, s::Sphere)
+function Base.in(p::StaticVector{Dim,T}, s::Sphere{Dim,T}) where {Dim,T}
     return norm(p - s.origin) ≤ s.radius
 end
 
 
-struct Disk <: AbstractShape
-    origin::Point3{Float64}
-    radius::Float64
-    height::Float64
+struct Disk{Dim,T} <: AbstractShape{Dim,T}
+    origin::StaticVector{Dim,T}
+    radius::T
+    height::T
 end
 
 
-function Base.in(p::Point, s::Disk)
+function Base.in(p::StaticVector{Dim,T}, s::Disk{Dim,T}) where {Dim,T}
     x, y, z = p - s.origin
     r, h = s.radius, s.height
-    return abs2(x) + abs2(y) <= abs2(r) && z >= -h/2 && z <= h/2
+    return abs2(x) + abs2(y) <= abs2(r) && z >= -h / 2 && z <= h / 2
 end
 
 
-struct Box <: AbstractShape
-    origin::Point3{Float64}
-    size::SVector{3,Float64}
+struct Box{Dim,T} <: AbstractShape{Dim,T}
+    origin::StaticVector{Dim,T}
+    size::SVector{Dim,T}
 end
 
-function Base.in(p::Point, s::Box)
+function Base.in(p::StaticVector{Dim,T}, s::Box{Dim,T}) where {Dim,T}
     return all(@. 0 ≤ p - s.origin ≤ s.size)
 end
 
 
-struct TriangularPlatelets <: AbstractShape
-    origin::Point3{Float64}
-    width::Float64
-    height::Float64
+struct TriangularPlatelets{Dim,T} <: AbstractShape{Dim,T}
+    origin::StaticVector{Dim,T}
+    width::T
+    height::T
 end
 
-function Base.in(p::Point, s::TriangularPlatelets)
+function Base.in(p::StaticVector{Dim,T}, s::TriangularPlatelets{Dim,T}) where {Dim,T}
     x, y, z = p - s.origin
     w, h = s.width, s.height
     return x >= -sqrt(3) / 4 * w && abs(y) <= w / 4 - x / sqrt(3) && z >= 0 && z <= h
 end
 
 
-struct Composite{I<:AbstractShape} <:AbstractShape
+struct Composite{Dim,T,I<:AbstractShape{Dim,T}} <: AbstractShape{Dim,T}
     shapes::Vector{I}
 end
 
@@ -69,40 +70,47 @@ Base.length(multi::Composite) = length(multi.shapes)
 Base.eltype(multi::Composite) = eltype(multi.shapes)
 Base.firstindex(multi::Composite) = firstindex(multi.shapes)
 Base.lastindex(multi::Composite) = lastindex(multi.shapes)
-Base.iterate(multi::Composite, state=1) =
-  state > length(multi) ? nothing : (multi[state], state+1)
+Base.iterate(multi::Composite, state = 1) =
+    state > length(multi) ? nothing : (multi[state], state + 1)
 
 
-function Base.in(p::Point, s::Composite)
+function Base.in(p::StaticVector{Dim,T}, s::Composite{Dim,T}) where {Dim,T}
     return mapreduce(s -> in(p, s), |, s)
 end
 
 
+function dipoles(g::CartesianGrid{Dim,T,V}, s::AbstractShape{Dim,T}) where {Dim,T,V}
 
-function dipoles(g::CartesianGrid{T,N}, s::AbstractShape) where {T,N}
+    coords = V[]
 
-    coords = Point{N,T}[]
-    inds = CartesianIndex{N}[]
-
-    for ind in CartesianIndices(g)
-        coord = g[ind]
-        if coord ∈ s
-            push!(coords, coord)
-            push!(inds, ind)
+    for i in eachindex(g)
+        coordinate = g[i]
+        if coordinate ∈ s
+            push!(coords, coordinate)
         end
     end
-    return coords, inds
+    return coords
 end
 
+function indices(g::CartesianGrid{Dim,T,V}, s::AbstractShape{Dim,T}) where {Dim,T,V}
 
+    inds = CartesianIndex{Dim}[]
 
-function discretize(g::CartesianGrid, s::S) where S<:AbstractShape
+    for i in eachindex(g)
+        coordinate = g[i]
+        if coordinate ∈ s
+            push!(inds, i)
+        end
+    end
+    return inds
+end
+
+function discretize(g::CartesianGrid, s::S) where {S<:AbstractShape}
 
     occupation = zeros(Bool, size(g))
 
     for i in eachindex(g)
         coordinate = g[i]
-
         if coordinate ∈ s
             occupation[i] = true
         end
