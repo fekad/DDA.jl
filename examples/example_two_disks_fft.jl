b8464f101d531c9448388764ef5d49a2fad77889
fekad <adam@fekete.co.uk>
2022-03-14 16:49:11 +0100

    cleanup

--- examples/example_two_disks_fft.jl
+++ examples/example_two_disks_fft.jl
@@ -1,3 +1,13 @@
+# target: Au disk
+# d = 10 nm
+# # of dipoles along the diameter: 30
+# height = 1 nm (3 dipoles)
+# grid spacing: d = 1/3 nm
+# # of dipoles: ??
+# gap: 4/3 nm
+# wavelength range: 632 732 nm
+
+
 
 using DDA
 
@@ -40,7 +50,7 @@ model = PermittivityTable(ustrip.(λ_exp), ε_exp)
 
 ##
 
-l = range(620, 740, step = 1.0)
+l = range(632, 732, step = 1.0)
 
 # Drude model
 # DOI: 10.1002/adom.201300457
@@ -227,11 +237,11 @@ function build_system()
 end
 
 
-function run_calc(lambda, model, verbose = false)
-    grid, inds, coords = build_system()
+function run_calc(grid, inds, coords, lambda, model, verbose = false)
+
 
     # Parameters
-    e = [0, 1]        # Jones polarisation vector
+    e = [1, 0]        # Jones polarisation vector
     θ, ϕ = 0.0, 0.0   # rotation angles [rad]
 
     k = 2π / lambda
@@ -250,29 +260,22 @@ function run_calc(lambda, model, verbose = false)
     prob = GridProblem(k, norm(e), grid, inds, alphas, Eincs)
 
     # 6. Solve the DDA problem
-    sol = solve(prob, BiCGStabl(), reltol = 1e-4, verbose = verbose)
-    return C_abs(sol), C_sca(sol)
+    sol = DDA.solve(prob, BiCGStabl(), reltol = 1e-4, verbose = verbose)
+    return sol
 
 end
-#
-#
-# to = TimerOutput()
-# @timeit to "main" begin
-#     Threads.@threads :static for i = 1:length(lambda_range)
-#         @show i
-#
-#         Q_abs[i], Q_sca[i] = run_calc(lambda_range[i], model)
-#
-#     end
-# end
-# print_timer(to)
+
 
 to = TimerOutput()
+
+grid, inds, coords = build_system()
+# P = zeros(SVector{3,Complex{Float64}}, length(inds))
+
 for i = eachindex(lambda_range)
     @timeit to "main" begin
         @show i
-
-        Q_abs[i], Q_sca[i] = run_calc(lambda_range[i], model)
+        sol = run_calc(grid, inds, coords, lambda_range[i], model, true)
+        Q_abs[i], Q_sca[i] = C_abs(sol), C_sca(sol)
     end
     print_timer(to)
 end
@@ -289,129 +292,6 @@ plot(trace, layout)
 
 
 
-plot(lambda_range, Q_abs, label = "abs");
-plot!(lambda_range, Q_sca, label = "sca");
-plot!(lambda_range, Q_abs + Q_sca, label = "ext")
-
-
-
-
-lambda_range = 620:1.0:740
-
-
-lamda_range
-
-
-
-
-
-
-
-
-
-# target: Au disk
-# d = 10 nm
-# # of dipoles along the diameter: 50
-# heightt = 1 nm (5 dipoles)
-# grid spacing: d = 0.2 nm
-# # of dipoles: ??
-# gap: d = (1) 0.2 nm, (2) 0.4 nm, (5) 1 nm,  (100) 20 nm
-# >> peak resonance at ??? nm
-# # of dipoles: 9880 ?
-# wavelength range: 620 740
-
-
-
-# 1. Define a grid
-# disk_r = 5. # nm
-
-
-diameter = 10.0 # nm
-height = 1.0 # nm
-
-gap = 1.0 # nm
-spacing = 0.2
-# sum(occ) = 19760
-
-gap = 4 / 3 # nm
-spacing = 1 / 3
-# sum(occ) = 4296
-
-Nx = round(Int, (2 * diameter + gap) / spacing)
-Ny = round(Int, diameter / spacing)
-Nz = round(Int, height / spacing)
-
-offset = [spacing / 2, spacing / 2, spacing / 2]
-origin = [-(2 * diameter + gap) / 2, -diameter / 2, -height / 2] .+ offset
-
-grid = CartesianGrid(origin, [spacing, spacing, spacing], (Nx, Ny, Nz))
-grid
-
-# 2. Define the target(s)
-origin = [(diameter + gap) / 2, 0, 0]
-
-d1 = DDA.Disk(-origin, diameter / 2, height)
-d2 = DDA.Disk(origin, diameter / 2, height)
-s = DDA.Composite([d1, d2])
-
-occ = DDA.discretize(grid, s)
-
-layout = Layout(yaxis = attr(scaleanchor = "x", scaleratio = 1))
-plot(heatmap(z = occ[:, :, 1]' .* 1, colorscale = "Viridis", xgap = 3, ygap = 3), layout)
-
-coords = grid[occ]
-
-trace = scatter3d(x = [p[1] for p in coords], y = [p[2] for p in coords], z = [p[3] for p in coords], marker = attr(color = 1:length(coords)), mode = "markers")
-layout = Layout(scene = attr(aspectmode = :data, camera_projection_type = :orthographic))
-plot(trace, layout)
-
-
-# 3. Define material
-
-# All in one together
-
-
-using Unitful
-using PhysicalConstants.CODATA2018: ε_0, c_0, h
-
-using RefractiveIndexDatabase
-using Permittivity
-
-λ_range = (632.0:1.0:732.0)u"nm"
-Au = get_material("main", "Au", "Babar")
-
-permittivity(material) = @. (material.n + material.k * im)^2
-# permittivity(material) = @. (material.n^2 - material.k^2) + (2 * material.n * material.k) * im
-wavelength(material) = material.λ * u"μm"
-
-ε = permittivity(Au)
-λ = wavelength(Au) .|> u"nm"
-
-int = QuadraticInterpolation(ε, λ)
-ε_int = int.(λ_range)
-
-#trace1 = scatter(x=ω_range, y=real(ε_int), name="Re(ε(ω))")
-trace2 = scatter(x = ω_range, y = imag(ε_int), name = "Im(ε(ω))")
-trace3 = scatter(x = ω, y = real(ε), name = "Re(ε)", mode = "markers")
-trace4 = scatter(x = ω, y = imag(ε), name = "Im(ε)", mode = "markers")
-
-layout1 = Layout(xaxis = attr(title = "[eV]", range = extrema(ω_range)), yaxis = attr(title = "ε", range = [-20, 0]))
-layout2 = Layout(xaxis = attr(title = "[eV]", range = extrema(ω_range)), yaxis = attr(title = "ε", range = [0, 1]))
-
-[plot([trace1, trace3], layout1) plot([trace2, trace4], layout2)]
-
-
-
-
-
-# 4. Define incindent field
-k = 2π     # wavenumber
-e = [1, 0]  # Jones polarisation vector
-θ, ϕ = 0.0, 0.0 # rotation angles [rad]
-
-
-E_inc = DDA.PlaneWave(k, e, θ, ϕ)
-E = DDA.field(E_inc, grid[occ])
 
 
 
